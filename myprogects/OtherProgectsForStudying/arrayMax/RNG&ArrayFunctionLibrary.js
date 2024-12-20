cd15d71551a9049185578cc0c7e7f41e685f767b
Ser0fimmm <129597508+Ser0fimmm@users.noreply.github.com>
2024-12-20 21:55:13 +0300

    Обновление для библиотеки функций для работы с массивами
    Добавлено:
    1. Определение, есть ли в массиве повторяющиеся значения.
    2. Вычисление кол-ва пар повторяющихся в массиве значений.
    3. Зачатки новой большой и сложной функции
    4. Универсальная функция заполнения массива случайными значениями на случай, если РНГ больше нигде в коде не нужен.
    5. Лучшее визуальное восприятие и новые пояснения
    Считайте это домашней работой за неделю)

--- myprogects/OtherProgectsForStudying/arrayMax/RNG&ArrayFunctionLibrary.js
+++ myprogects/OtherProgectsForStudying/arrayMax/RNG&ArrayFunctionLibrary.js
@@ -1,30 +1,35 @@
-function RandomNumberGnerator(min,max) {
+function RandomNumberGnerator(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
-}// RNG
+}// basic RNG
 function FillArray(array, min, max) { 
     for (let index = 0; index < array.length; index++) {
         array[index] = RandomNumberGnerator(min, max) 
     }
-}// Fills up empty array with random numbers
-function SortArrayEven(ArrayForSort, EvenArray,) { 
+}// Fills up empty array with random numbers. \ the array must exist with a certain number of elements. \ examplle: arrray = new Array(n), where n = array lenght
+function RandomlyFillArray(array, min, max){
+    for (let index = 0; index < array.length; index++) {
+        array[index] = Math.floor(Math.random() * (max - min) + min) 
+    }
+}// does absolutly the same operation as the previous FillArray function, but without using RNG Function. use one of the options, depending on whether you use RNG in your code or not. 
+function SortArrayEven(ArrayForSort, EvenArray) { 
     for (let index = 0; index < ArrayForSort.length; index++) {
         const element = ArrayForSort[index];
-        if (element % 2 == 0){
+        if (element % 2 == 0) {
             EvenArray.push(ArrayForSort[index]);
         }  
     }
 } // Sorts array for even numbers to new array
-function SortArrayNotEen(ArrayForSort,NotEvenArray){
+function SortArrayNotEven(ArrayForSort, NotEvenArray){
     for (let index = 0; index < ArrayForSort.length; index++) {
         const element = ArrayForSort[index];
-        if (element % 2 != 0){
+        if (element % 2 != 0) {
             NotEvenArray.push(ArrayForSort[index]); 
         }  
     }
 }// Sorts array for not even numbers to new array
-function ArraySortForEvenAndNotEven(ArrayForSort, EvenArray,NotEvenAray) {
-    SortArrayEven(ArrayForSort,EvenArray); 
-    SortArrayNotEen(ArrayForSort,NotEvenAray);  
+function ArraySortForEvenAndNotEven(ArrayForSort, EvenArray, NotEvenAray) {
+    SortArrayEven(ArrayForSort, EvenArray); 
+    SortArrayNotEen(ArrayForSort, NotEvenAray);  
 }// Sorts array for even and not even numbers to two new arrays
 function FindMaxInArray(Array){
     for (let index = 0; index < Array.length; index++) {
@@ -35,7 +40,7 @@ function FindMaxInArray(Array){
         }
         return Maxelement;
     }
-}// Returns maximum value in Array
+}// Returns maximum value in Array 
 function FindMinInArray(Array){
     for (let index = 0; index < Array.length; index++) {
         const element = Array[index];
@@ -46,3 +51,35 @@ function FindMinInArray(Array){
         return MinElement;
     }
 }// Returns minimum value in Array
+function RepeatingNumbersExistanceInArray(array) {
+    let i = 0;
+    array.sort((a, b) => a - b);
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const element2 = array[index + 1];
+        if (element == element2) {
+            i++;
+            break;
+        }
+        else if(element != element2) {
+            continue;
+        }   
+    }
+    if (i >= 1) return true;
+    else if (i == 0) return false;
+}// returns true if array has repeating numbers and false if it does'nt
+function NumberOfRepeatingNumbersInArray(array) {
+    let i = 0;
+    array.sort((a, b) => a - b);
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const element2 = array[index + 1];
+        if (element == element2) {
+            i ++;
+        }
+        else if(element != element2) {
+            continue;
+        }   
+    }
+    return i;
+}// returns the number of pairs of the repeating numbers in array
