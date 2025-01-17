61863a600a18647ed551c41467c14a95d8417ccd
Ser0fimmm <129597508+Ser0fimmm@users.noreply.github.com>
2025-01-18 00:52:14 +0300

    новая работающая игра "найди пару" и готовый к сдаче проектов основной сайт
    ГОСПОДИ НУ НАКОНЕЦ-ТО
    ЧАС НОЧИ А Я СИЖУ, ПИШУ ПОЛНОСТЬЮ НОВЫЙ КОД ДЛЯ ИГРЫ "НАЙДИ ПАРУ". Я СПАТЬ ХОЧУ УЖАС КАК.

--- myprogects/FindPairs/code.js
+++ myprogects/FindPairs/code.js
@@ -1,79 +1,221 @@
-let cards = [
-    { id: 1, image: 'image1.png' },
-    { id: 1, image: 'image1.png' },
-    {id: 2, image: 'image2.png' },
-    { id: 2, image: 'image2.png' },
-    {id: 3, image: 'image3.png' },
-    { id: 3, image: 'image3.png' },
-    {id: 4, image: 'image4.png' },
-    { id: 4, image: 'image4.png' },
-    // Добавить все карты по аналогии
-    ];
-cards = shuffle(cards);
-function shuffle(array) {
-array.sort(() => Math.random() - 0.5);
-}
-function renderCards(cards) {
-    const gameContainer = document.getElementById('game');
-    cards.forEach(card => {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-    // Добавление обработчика клика
-    cardElement.addEventListener('click', () => handleCardClick(card));
-    gameContainer.appendChild(cardElement);
-    });
-}
-function handleCardClick(card) {
-    console.log(card);
-    // Реализация логики выбора карты и проверки на совпадение
-}
-renderCards(cards);
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-function flipCard(card) {
-if (lockBoard) return;
-if (card === firstCard) return;
-card.classList.add('flip');
-if (!hasFlippedCard) {
-hasFlippedCard = true;
-firstCard = card;
-return;
-}
-secondCard = card;
-checkForMatch();
-}
-function checkForMatch() {
-    let isMatch = firstCard.dataset.id === secondCard.dataset.id;
-    isMatch ? disableCards() : unflipCards();
-}
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-    resetBoard();
-}
-function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-    resetBoard();
-}, 1000);
-}
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-}
-function checkWin() {
-    if (document.querySelectorAll('.card.flip').length === cards.length) {
-        setTimeout(() => {
-        alert('Поздравляем! Вы нашли все пары!');
-        resetGame();
-        }, 500);
-    }
-}
-function resetGame() {
-    document.getElementById('game').innerHTML = '';
-    cards = shuffle(cards);
-    renderCards(cards);
-}
+// Memory Game
+// © 2014 Nate Wiley
+// License -- MIT
+
+// весь скрипт — это одна большая функция
+(function(){
+	
+	//  объявляем объект, внутри которого будет происходить основная механика игры
+	var Memory = {
+
+		// создаём карточку
+		init: function(cards){
+			//  получаем доступ к классам
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			// собираем из карточек массив — игровое поле
+			this.cardsArray = $.merge(cards, cards);
+			// перемешиваем карточки
+			this.shuffleCards(this.cardsArray);
+			// и раскладываем их
+			this.setup();
+		},
+
+		// как перемешиваются карточки
+		shuffleCards: function(cardsArray){
+			// используем встроенный метод .shuffle
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
+
+		// раскладываем карты
+		setup: function(){
+			// подготавливаем код с карточками на страницу
+			this.html = this.buildHTML();
+			// добавляем код в блок с игрой
+			this.$game.html(this.html);
+			// получаем доступ к сформированным карточкам
+			this.$memoryCards = $(".card");
+			// на старте мы не ждём переворота второй карточки
+			this.paused = false;
+			// на старте у нас нет перевёрнутой первой карточки
+     		this.guess = null;
+     		// добавляем элементам на странице реакции на нажатия
+			this.binding();
+		},
+
+		// как элементы будут реагировать на нажатия
+		binding: function(){
+			// обрабатываем нажатие на карточку
+			this.$memoryCards.on("click", this.cardClicked);
+			// и нажатие на кнопку перезапуска игры
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+
+		// что происходит при нажатии на карточку
+		cardClicked: function(){
+			// получаем текущее состояние родительской переменной
+			var _ = Memory;
+			// и получаем доступ к карточке, на которую нажали
+			var $card = $(this);
+			// если карточка уже не перевёрнута и мы не нажимаем на ту же самую карточку второй раз подряд
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				// переворачиваем её
+				$card.find(".inside").addClass("picked");
+				// если мы перевернули первую карточку
+				if(!_.guess){
+					// то пока просто запоминаем её
+					_.guess = $(this).attr("data-id");
+				// если мы перевернули вторую и она совпадает с первой
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					// оставляем обе на поле перевёрнутыми и показываем анимацию совпадения
+					$(".picked").addClass("matched");
+					// обнуляем первую карточку
+					_.guess = null;
+						// если вторая не совпадает с первой
+						} else {
+							// обнуляем первую карточку
+							_.guess = null;
+							// не ждём переворота второй карточки
+							_.paused = true;
+							// ждём полсекунды и переворачиваем всё обратно
+							setTimeout(function(){
+								$(".picked").removeClass("picked");
+								Memory.paused = false;
+							}, 600);
+						}
+				// если мы перевернули все карточки
+				if($(".matched").length == $(".card").length){
+					// показываем победное сообщение
+					_.win();
+				}
+			}
+		},
+
+		// показываем победное сообщение
+		win: function(){
+			// не ждём переворота карточек
+			this.paused = true;
+			// плавно показываем модальное окно с предложением сыграть ещё
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		// показываем модальное окно
+		showModal: function(){
+			// плавно делаем блок с сообщением видимым
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
+
+		// прячем модальное окно
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
+
+		// перезапуск игры
+		reset: function(){
+			// прячем модальное окно с поздравлением
+			this.hideModal();
+			// перемешиваем карточки
+			this.shuffleCards(this.cardsArray);
+			// раскладываем их на поле
+			this.setup();
+			// показываем игровое поле
+			this.$game.show("slow");
+		},
+
+		// Тасование Фишера–Йетса - https://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+		   	while (counter > 0) {
+	        	index = Math.floor(Math.random() * counter);
+	        	counter--;
+	        	temp = array[counter];
+	        	array[counter] = array[index];
+	        	array[index] = temp;
+		    	}
+		    return array;
+		},
+
+		// код, как добавляются карточки на страницу
+		buildHTML: function(){
+			// сюда будем складывать HTML-код
+			var frag = '';
+			// перебираем все карточки подряд
+			this.$cards.each(function(k, v){
+				// добавляем HTML-код для очередной карточки
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="https://avatars.mds.yandex.net/i?id=b3f6ab2cc6a752274564c63269c4af7df36f81dd-9067430-images-thumbs&n=13"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			// возвращаем собранный код
+			return frag;
+		}
+	};
+
+	// карточки
+	var cards = [
+		{	
+			// название
+			name: "php",
+			// адрес картинки
+			img: "image1.png",
+			// порядковый номер пары
+			id: 1,
+		},
+		{
+			name: "css3",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/160px-CSS3_logo.svg.png",
+			id: 2
+		},
+		{
+			name: "html5",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/160px-HTML5_logo_and_wordmark.svg.png",
+			id: 3
+		},
+		{
+			name: "jquery",
+			img: "image2.png",
+			id: 4
+		}, 
+		{
+			name: "javascript",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/160px-Unofficial_JavaScript_logo_2.svg.png",
+			id: 5
+		},
+		{
+			name: "node",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/262px-Node.js_logo.svg.png",
+			id: 6
+		},
+		{
+			name: "photoshop",
+			img: "image3.png",
+			id: 7
+		},
+		{
+			name: "python",
+			img: "image4.png",
+			id: 8
+		},
+        {
+			name: "rails",
+			img: "https://i.pinimg.com/736x/12/c0/5f/12c05f08d3ea1f424bb64d0e1125ea7f.jpg",
+			id: 9
+		},
+        
+	];
+    
+	// запускаем игру
+	Memory.init(cards);
+
+
+})();
