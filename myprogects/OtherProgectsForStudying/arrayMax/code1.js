7a0a47b5610f6e5cfaa0d962823ad7dd6bcc0d05
Artyom <129597508+Ser0fimmm@users.noreply.github.com>
2025-01-25 17:01:05 +0300

    Обновление: доступен массив со случайными значениями

--- myprogects/OtherProgectsForStudying/arrayMax/code1.js
+++ myprogects/OtherProgectsForStudying/arrayMax/code1.js
@@ -4,40 +4,70 @@ let rngevenarray = []
 let oddarray = []
 let rngoddarray =[]
 let rnsarray =[]
-let rngarray = new Array()
+
 let repeatingpairs = 0
+
+
+
 function addnum() {
+    
     userarray.push(document.getElementById("input").value);
     document.getElementById("urarray").textContent = `Ваш массив: ${userarray}.`;
 }
 
-function SortArrayEven(ArrayForSort, EvenArray) { 
-    for (let index = 0; index < ArrayForSort.length; index++) {
-        const element = ArrayForSort[index];
-        if (element % 2 == 0) {
-            EvenArray.push(ArrayForSort[index]);
-        }  
-    }
-    document.getElementById("myevenarray").textContent = `Четные значения: ${evenarray}.`;
-}
-function SortArrayOdd(ArrayForSort, OddArray){
-    for (let index = 0; index < ArrayForSort.length; index++) {
-        const element = ArrayForSort[index];
-        if (element % 2 != 0) {
-            OddArray.push(ArrayForSort[index]); 
-        }  
-    }
-    document.getElementById("uroddarray").textContent = `Нечетные значения: ${oddarray}.`
-}
-function FindMaxInArray(Array){
-    for (let index = 0; index < Array.length; index++) {
-        const element = Array[index];
-        let Maxelement = 0;
-        if (element > Maxelement) { 
-            Maxelement = element;
+function SortArrayEven() { 
+    let conf = confirm("OK, если свой массив | NO / Отмена, если случайный массив");
+    if (conf == true) {
+        for (let index = 0; index < userarray.length; index++) {
+            const element = userarray[index];
+            if (element % 2 == 0) {
+                evenarray.push(userarray[index]);
+            }  
         }
-        document.getElementById("max").textContent = `Максимальное значение: ${Maxelement}.`;
+        document.getElementById("myevenarray").textContent = `Четные значения: ${evenarray}.`;
     }
+    else if (conf == false) {
+        for (let index1 = 0; index1 < rngarray.length; index1++) {
+            const element1 = rngarray[index1];
+            if (element1 % 2 == 0) {
+                rngevenarray.push(rngarray[index1]);
+            }  
+        }
+        document.getElementById("randomeven").textContent = `Четные значения: ${rngevenarray}.`;
+    }
+    
+}
+function SortArrayOdd(){
+    let conf = confirm("OK, если свой массив | NO / Отмена, если случайный массив");
+    if (conf == true) {
+        for (let index = 0; index < userarray.length; index++) {
+            const element = userarray[index];
+            if (element % 2 != 0) {
+                oddarray.push(userarray[index]); 
+            }  
+        }
+        document.getElementById("uroddarray").textContent = `Нечетные значения: ${oddarray}.`
+    }
+    else if (conf == false) {
+        for (let index1 = 0; index1 < rngarray.length; index1++) {
+            const element1 = rngarray[index1];
+            if (element1 % 2 != 0) {
+                rngoddarray.push(rngarray[index1]); 
+            }  
+        }
+        document.getElementById("rngoddarray").textContent = `Нечетные значения: ${rngoddarray}.`
+    }
+}
+function FindMaxInArray(){
+    let conf = confirm("OK, если свой массив | NO / Отмена, если случайный массив");
+    if (conf == true) {
+        maximum = Math.max.apply(null, userarray);
+        document.getElementById("max").textContent = `Максимальное значение: ${maximum}.`;
+    }
+    else if (conf == false) {
+        maximum1 = Math.max.apply(null, rngarray);
+        document.getElementById("maxrng").textContent = `Максимальное значение: ${maximum1}.`;
+}
 }
 function FindMinInArray(Array){
     for (let index = 0; index < Array.length; index++) {
@@ -64,4 +94,41 @@ function NumberOfRepeatingNumbersInArray(array) {
     }
     if (i == 0) document.getElementById("rpn").textContent = `Нет пар повторяющихся значений.`
     else document.getElementById("rpn").textContent = `${i} - кол-во пар повторяющихся значений.`
+}
+
+let usermin 
+let usermax
+let useramount
+
+let rngarray 
+
+function RandomNumberGnerator(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function FillArray(array, min, max) {
+    for (let index = 0; index < array.length; index++) {
+        array[index] = RandomNumberGnerator(min, max) 
+        
+    }
+}
+
+function randomarray() {
+    if (document.getElementById("randomarray").textContent = "Сгенерировать") {
+        usermin = +prompt("Введите минимальное значение");
+        usermax = +prompt("ВВедите максимальное значение");
+        useramount = +prompt("Введите количество элементов массива");
+        rngarray = new Array(useramount);
+        FillArray(rngarray, usermin, usermax);
+        document.getElementById("rngarray").textContent = `Сгенерированный массив: ${rngarray}`;
+        document.getElementById("randomarray").textContent = "Сгенерировать новый";
+    }
+    else {
+        usermin = +prompt("Введите минимальное значение");
+        usermax = +prompt("ВВедите максимальное значение");
+        useramount = +prompt("Введите количество элементов массива")
+        rngarray = new Array(useramount);
+        FillArray(rngarray, usermin, usermax);
+        document.getElementById("rngarray").textContent = `Сгенерированный массив: ${rngarray}`
+    }
 }
