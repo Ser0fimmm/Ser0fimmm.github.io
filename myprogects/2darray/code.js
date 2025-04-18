7b901751f9be67ac9abc90f4ef10ccf5e920b498
Ser0fimmm <129597508+Ser0fimmm@users.noreply.github.com>
2025-04-18 20:03:09 +0300

    find closest elements (arr)

--- myprogects/2darray/code.js
+++ myprogects/2darray/code.js
@@ -33,3 +33,26 @@ function findTreasure(array) {
 }
 showMaze(arr)
 findTreasure(arr)
+
+function findClosestElements(arr) {
+    if (arr.length < 2) {
+        return []; // Массив должен содержать как минимум 2 элемент
+    }
+    let minDiff = Math.abs(arr[1] - arr[0]);
+    let result = [0, 1];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            const currentDiff = Math.abs(arr[j] - arr[i]);
+            if (currentDiff < minDiff) {
+                minDiff = currentDiff;
+                result = [arr[i], arr[j]];
+            }
+        }
+    }
+    // Возвращаем номера в порядке возрастания (на случай если i > j)
+    return result.sort((a, b) => a - b);
+}
+
+const array = [4, 31, 52, 97, 10, 43];
+const result = findClosestElements(array);
+console.log(" bbw" + result); // 4, 10
