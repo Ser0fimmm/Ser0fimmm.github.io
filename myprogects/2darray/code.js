3da2c7fd6100fa3a454a22be072c0a201144f031
Ser0fimmm <129597508+Ser0fimmm@users.noreply.github.com>
2025-04-26 16:59:16 +0300

    ну оно почти работает но нет(

--- myprogects/2darray/code.js
+++ myprogects/2darray/code.js
@@ -1,6 +1,6 @@
 let arr = [
 [5,12,9],
-[14,18,8],
+[14,26,8],
 [21,24,7],
 ]
 
@@ -103,3 +103,81 @@ function findMaxInColumns(matrix) {
     return maxInColumns;
 }
 console.log(findMaxInColumns(arr))
+
+
+function findmaxin2darray(array) {
+    let max = array[0][0]
+    let maxcol 
+    let maxrow
+    
+    for (let col = 0; col < array.length; col++) {
+        for (let row = 0; row < array[0].length; row++) {
+            const element = array[row][col];
+            if (element == max) {continue}
+            if (element > max) {maxcol = col
+                                maxrow = row}
+
+        }
+        
+    }
+    let result = [maxcol, maxrow]
+    return result
+} 
+console.log(findmaxin2darray(arr))
+//document.getElementById("ddddd").textContent = `lol${findmaxin2darray(arr)}`
+function findlocalmax(array) {
+   
+    for (let col = 1; col < array.length - 1; col++) {
+        for (let row = 1; row < array[0].length - 1; row++) {
+            
+            if (array[row][col] > array[row + 1][col]
+                 && array[row][col] > array[row - 1][col]
+                  && array[row][col] > array[row][col + 1]
+                   && array[row][col] > array[row][col - 1]
+                    && array[row][col] > array[row + 1][col + 1]
+                     && array[row][col] > array[row - 1][col + 1]
+                      && array[row][col] > array[row + 1][col - 1]
+                       && array[row][col] > array[row - 1][col - 1]){
+                    array[row][col] = 0
+            }
+        }
+    }
+    return array
+}
+let lox = 
+[[3, -2, -5, -5, 8, -4, -1, -23, -11, -20], 
+[-4, -6, 16, 9, -8, 2, 10, -32, -18, -13], 
+[21, 7, -6, -3, 34, -7, -23, 25, 27, 38], 
+[7, 29, 38, 19, 55, 48, 46, 49, -8, -46], 
+[80, -3, 76, 46, 72, -33, 14, 44, 71, -20], 
+[82, 95, 23, -6, 1, 70, 50, 16, 25, 7], 
+[52, 36, -25, -35, 18, -36, 93, 11, -44, 9], 
+[114, 108, 33, 61, -4, 131, 34, -26, 47, -17], 
+[37, 56, 135, -12, 34, 67, -17, 14, 93, -67], 
+[-4, 23, 175, -44, 30, -38, 89, 159, 28, 178]]
+let lox2 = findlocalmax(lox)
+console.log(lox2)
+let arr1 = findlocalmax(arr)
+function findlocalmaxtest(array, array1) {
+   let test  = false
+    for (let col = 1; col < array.length - 1; col++) {
+        for (let row = 1; row < array[0].length - 1; row++) {
+            
+            if (array[row][col] > array[row + 1][col]
+                 && array[row][col] > array[row - 1][col]
+                  && array[row][col] > array[row][col + 1]
+                   && array[row][col] > array[row][col - 1]
+                    && array[row][col] > array[row + 1][col + 1]
+                     && array[row][col] > array[row - 1][col + 1]
+                      && array[row][col] > array[row + 1][col - 1]
+                       && array[row][col] > array[row - 1][col - 1]
+                        && array1[row][col] == 0){
+                    test = true
+            }
+           
+            
+        }
+    }
+    return test
+}
+console.log(findlocalmaxtest(arr, arr1))  
